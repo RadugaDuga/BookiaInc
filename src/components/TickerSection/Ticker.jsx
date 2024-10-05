9ab9e8041b6616d635f7d84c197e2a77dda025ff
Georgi Bukia <okretans@mail.ru>
2024-10-05 22:51:32 +0400

    Added new Tickers

--- src/components/TickerSection/Ticker.jsx
+++ src/components/TickerSection/Ticker.jsx
@@ -1,39 +1,20 @@
 import "./TickerSection.css";
 
-const Ticker = () => {
+const Ticker = ({ items, direction = 'left' }) => {
 	return (
-		<div className="stock-ticker">
-			<ul>
-				<li className="minus">Effector</li>
-				<li className="plus">Redux</li>
-				<li className="plus">React</li>
-				<li className="minus">Axios</li>
-				<li className="plus">WebPack</li>
-				<li className="minus">NPM</li>
-				<li className="plus">Lodash</li>
-				<li className="minus">Vite</li>
-				<li className="minus">Yarn</li>
-				<li className="minus">Vue</li>
-				<li className="minus">Jinja</li>
-				<li className="minus">RabbitMQ</li>
-			</ul>
-
-			<ul aria-hidden="true">
-				<li className="minus">Effector</li>
-				<li className="plus">Redux</li>
-				<li className="plus">React</li>
-				<li className="minus">Axios</li>
-				<li className="plus">WebPack</li>
-				<li className="minus">NPM</li>
-				<li className="plus">Lodash</li>
-				<li className="minus">Vite</li>
-				<li className="minus">Yarn</li>
-				<li className="minus">Vue</li>
-				<li className="minus">Jinja</li>
-				<li className="minus">RabbitMQ</li>
-			</ul>
-		</div>
+	  <div className={`stock-ticker-${direction}`}>
+		<ul>
+		  {items.map((item, index) => (
+			<li key={index} className={item.type}>{item.name}</li>
+		  ))}
+		</ul>
+		<ul aria-hidden="true">
+		  {items.map((item, index) => (
+			<li key={index + items.length} className={item.type}>{item.name}</li>
+		  ))}
+		</ul>
+	  </div>
 	);
-};
+  };
 
 export default Ticker;
--- src/components/TickerSection/TickerSection.jsx
+++ src/components/TickerSection/TickerSection.jsx
@@ -1,7 +1,60 @@
 import Ticker from "./Ticker";
 
+const items = [
+	{ name: "Effector" },
+	{ name: "Redux" },
+	{ name: "React" },
+	{ name: "Axios" },
+	{ name: "WebPack" },
+	{ name: "NPM" },
+	{ name: "Lodash" },
+	{ name: "Vite" },
+	{ name: "Yarn" },
+	{ name: "Javascript" },
+	{ name: "Jinja" },
+	{ name: "RabbitMQ" },
+];
+
+const items2 = [
+	{ name: "HTML5" },
+	{ name: "CSS3" },
+	{ name: "ThreeJS" },
+	{ name: "Gsap" },
+	{ name: "Locomotive" },
+	{ name: "Keykloak" },
+	{ name: "Jest" },
+	{ name: "Docker" },
+	{ name: "NodeJs" },
+	{ name: "Yarn" },
+	{ name: "jQuery" },
+	{ name: "SASS" },
+	{ name: "SCSS" },
+];
+
+const items3 = [
+	{ name: "Redux-Thunk" },
+	{ name: "Vue" },
+	{ name: "Redux-Saga" },
+	{ name: "RxJs" },
+	{ name: "Formik" },
+	{ name: "Typescript" },
+	{ name: "MaterialUI" },
+	{ name: "Tailwind" },
+	{ name: "ReactVirtualized" },
+	{ name: "Enzyme" },
+	{ name: "React-PropTypes" },
+	{ name: "Swagger" },
+	{ name: "Postman" },
+];
+
 function TickerSection() {
-	return <Ticker />;
+	return (
+		<>
+			<Ticker items={items} direction="right" />
+			<Ticker items={items2} direction="left" />
+			<Ticker items={items3} direction="right" />
+		</>
+	);
 }
 
 export default TickerSection;
