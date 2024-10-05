d125302cfcd2047af05f31a91c4d9e77fcc87df8
Georgi Bukia <okretans@mail.ru>
2024-10-05 04:07:25 +0400

    Added reverse ticker css

--- src/components/TickerSection/Ticker.jsx
+++ src/components/TickerSection/Ticker.jsx
@@ -4,57 +4,33 @@ const Ticker = () => {
 	return (
 		<div className="stock-ticker">
 			<ul>
-				<li className="minus">
-					Effector
-				</li>
-				<li className="plus">
-					Redux
-				</li>
-				<li className="plus">
-					React
-				</li>
-				<li className="minus">
-					Axios
-				</li>
-				<li className="plus">
-					WebPack
-				</li>
-				<li className="minus">
-					NPM
-				</li>
-				<li className="plus">
-					Lodash
-				</li>
-				<li className="minus">
-					Date-Fns
-				</li>
+				<li className="minus">Effector</li>
+				<li className="plus">Redux</li>
+				<li className="plus">React</li>
+				<li className="minus">Axios</li>
+				<li className="plus">WebPack</li>
+				<li className="minus">NPM</li>
+				<li className="plus">Lodash</li>
+				<li className="minus">Vite</li>
+				<li className="minus">Yarn</li>
+				<li className="minus">Vue</li>
+				<li className="minus">Jinja</li>
+				<li className="minus">RabbitMQ</li>
 			</ul>
 
 			<ul aria-hidden="true">
-				<li className="minus">
-					Effector
-				</li>
-				<li className="plus">
-					Redux
-				</li>
-				<li className="plus">
-					React
-				</li>
-				<li className="minus">
-					Axios
-				</li>
-				<li className="plus">
-					WebPack
-				</li>
-				<li className="minus">
-					NPM
-				</li>
-				<li className="plus">
-					Lodash
-				</li>
-				<li className="minus">
-					Date-Fns
-				</li>
+				<li className="minus">Effector</li>
+				<li className="plus">Redux</li>
+				<li className="plus">React</li>
+				<li className="minus">Axios</li>
+				<li className="plus">WebPack</li>
+				<li className="minus">NPM</li>
+				<li className="plus">Lodash</li>
+				<li className="minus">Vite</li>
+				<li className="minus">Yarn</li>
+				<li className="minus">Vue</li>
+				<li className="minus">Jinja</li>
+				<li className="minus">RabbitMQ</li>
 			</ul>
 		</div>
 	);
