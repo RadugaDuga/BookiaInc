d602f600f1209c85853e77054087215fdad738b8
RadugaDuga <okretans@mail.ru>
2025-05-17 03:32:43 +0400

    feat: add Easter egg console log in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,17 @@ import ArtsSection from "./sections/ArtsSection";
 import ContactsSection from "./sections/ContactsSection";
 import NewTicker from "./sections/TickerSection";
 import ScrollToBottom from "./components/ScrollToBottom";
+import { useEffect } from "react";
 
 function App() {
+	// Пасхалка :D
+	useEffect(() => {
+		console.log(
+			"%cА вот и пасхалка, держи анекдот!\n— Почему программисты предпочитают темную тему ?\n— ... \n— Потому что светлая привлекает насекомых 🥁🥁",
+			"background: rgb(189, 26, 91); color: white; margin-left:6vw; padding: 6px 12px; font-size: 1.3em; margin-bottom:5px"
+		);
+	}, []);
+
 	return (
 		<div className="section">
 			<PreviewSection />
